a835ce11a3f18a7ae2d4edc1988a9aa9a9eb2888
Yeet <113854826+YeeetSK@users.noreply.github.com>
2024-04-29 14:23:35 +0200

    Textui update

--- client/client.lua
+++ client/client.lua
@@ -22,43 +22,71 @@ for k, v in ipairs(Config.StashLocs) do
         end
     end)
 
-
-    if Config.Target == 'ox' then
-        exports.ox_target:addBoxZone({
-            coords = vector3(v.coords.x, v.coords.y, v.coords.z - 1),
-            size = vector3(3.0, 3.0, 3.0),
-            rotation = 0.0,
-            debug = false,
-            options = {
+    if Config.OpenType == 'target' then
+        if Config.Target == 'ox' then
+            exports.ox_target:addBoxZone({
+                coords = vector3(v.coords.x, v.coords.y, v.coords.z + 0.25),
+                size = vector3(3.0, 3.0, 3.0),
+                rotation = 0.0,
+                debug = false,
+                options = {
+                    {
+                        type = "client",
+                        event = 'yetticodeStash:openStash'.. v.id,
+                        icon = Config.TargetIcon,
+                        label = Config.TargetLabel,
+                        distance = Config.Distance,
+                    }
+                }
+            })
+        elseif Config.Target == 'qb' then
+            exports['qb-target']:AddBoxZone(v.id, vector3(v.coords.x, v.coords.y, v.coords.z + 0.25), 1.5, 1.6, {
+                name = v.id,
+                heading = 0,
+                debugPoly = Config.Debug,
+                minZ = v.coords.z - 1.5,
+                maxZ = v.coords.z + 1.5,
+            }, {
+                options = {
                 {
-                    type = "client",
-                    event = 'yetticodeStash:openStash'.. v.id,
                     icon = Config.TargetIcon,
                     label = Config.TargetLabel,
-                    distance = 3,
+                    action = function ()
+                        TriggerEvent('yetticodeStash:openStash' .. v.id)
+                    end
                 }
-            }
-        })
-    elseif Config.Target == 'qb' then
-        exports['qb-target']:AddBoxZone(v.id, vector3(v.coords.x, v.coords.y, v.coords.z - 1), 1.5, 1.6, {
-            name = v.id,
-            heading = 0,
-            debugPoly = Config.Debug,
-            minZ = v.coords.z - 1.5,
-            maxZ = v.coords.z + 1.5,
-        }, {
-            options = {
-            {
-                icon = Config.TargetIcon,
-                label = Config.TargetLabel,
-                action = function ()
-                    TriggerEvent('yetticodeStash:openStash' .. v.id)
+                },
+                distance = Config.Distance,
+            })
+        end
+
+    elseif Config.OpenType == 'textui' then
+        local point = lib.points.new({
+            coords = vector3(v.coords.x, v.coords.y, v.coords.z + 0.25),
+            distance = Config.Distance,
+            onExit = function()
+                if lib.isTextUIOpen() then
+                    lib.hideTextUI()
                 end
-            }
-            },
-            distance = Config.TargetDistance,
+            end
         })
+
+
+        function point:nearby()
+            if not lib.isTextUIOpen() then
+                lib.showTextUI(locale("open_stash_textui"))
+            end
+
+            if IsControlJustPressed(0, 51) then
+                TriggerEvent('yetticodeStash:openStash' .. v.id)
+            end
+        end
+
+
+
     end
+
+    
 
 
 end
